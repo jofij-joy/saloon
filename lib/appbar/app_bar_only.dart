d92a64dce0e0f0f1c39b739b46e0364a8d1d6177
jofij <jofijjoychungan01@gmail.com>
2022-07-08 17:00:11 +0530

    Saloon App

---
+++ lib/appbar/app_bar_only.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:saloon/constant/color_constant.dart';
+import 'package:saloon/constant/constant_font.dart';
+
+Widget appbar(
+    BuildContext context, String title, dynamic otherData, bool appon) {
+  final GlobalKey<ScaffoldState> _drawerscaffoldkey = otherData;
+
+  return AppBar(
+    backgroundColor: whiteColor,
+    systemOverlayStyle: SystemUiOverlayStyle.light,
+    centerTitle: true,
+    elevation: 0.0,
+    iconTheme: new IconThemeData(color: blackColor),
+    title: Text(
+      title,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+          color: blackColor,
+          fontFamily: ConstantFont.montserratBold,
+          fontSize: 15,
+          fontWeight: FontWeight.w600),
+    ),
+    leading: IconButton(
+      onPressed: () {
+        if (_drawerscaffoldkey.currentState!.isDrawerOpen) {
+          Navigator.pop(context);
+        } else {
+          _drawerscaffoldkey.currentState!.openDrawer();
+        }
+      },
+      icon: Icon(Icons.menu),
+    ),
+  );
+}
---
+++ lib/network/Retro_Api.dart
@@ -0,0 +1,23 @@
+
+
+import 'package:saloon/constant/appconstant.dart';
+import 'package:saloon/constant/preferenceutils.dart';
+import 'package:dio/dio.dart';
+import 'package:saloon/constant/appconstant.dart';
+import 'package:saloon/constant/preferenceutils.dart';
+
+class RetroApi {
+
+  Dio dioData()
+   {
+    final dio = Dio();
+
+
+    dio.options.headers["Authorization"] =  "Bearer"+"  "+PreferenceUtils.getString(AppConstant.headertoken);   // config your dio headers globally
+    dio.options.headers["Accept"] = "application/json";   // config your dio headers globally
+    dio.options.headers["Content-Type"] = "application/x-www-form-urlencoded";
+    dio.options.followRedirects = false;
+
+    return dio;
+   }
+}
